157e1cc366175c440255de6a47bb2f5e323d0311
Francisco Zamora <francisco.zamora109010@gmail.com>
2020-01-30 09:45:21 -0800

    Dployed Lmbda, added token sys perm for tbl access

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from url_shortener.url_shortener_stack import UrlShortenerStack
 
 
 app = core.App()
-UrlShortenerStack(app, "url-shortener")
+UrlShortenerStack(app, "url-shortener", env=core.Environment(region="us-east-1",account="431528647878"))
 
 app.synth()
--- url_shortener/url_shortener_stack.py
+++ url_shortener/url_shortener_stack.py
@@ -1,5 +1,6 @@
-from aws_cdk import core, aws_dynamodb
+from aws_cdk import core, aws_dynamodb, aws_lambda
 
+# pip install -e .  ; install requirements
 
 class UrlShortenerStack(core.Stack):
 
@@ -8,6 +9,27 @@ class UrlShortenerStack(core.Stack):
 
         # The code that defines your stack goes here
         # creating DynamoDb table
-        table = aws_dynamodb.Table(self, "mapping-table", 
-                                partition_key=aws_dynamodb.Attribute(name="id", type=aws_dynamodb.AttributeType.STRING))
-# cdk deploy --profile-workProfile to deploy resource onto my account
+        table = aws_dynamodb.Table(self, "mapping_table", 
+                                    # partition key required
+                                    partition_key=aws_dynamodb.Attribute(name="id", 
+                                    type=aws_dynamodb.AttributeType.STRING)
+                                    )
+# cdk deploy --profile workProfile   ;to deploy resource onto my account with workProfile Credentials
+
+        # create lambda resource
+        function = aws_lambda.Function(self, "backend_lambda",
+                                        runtime=aws_lambda.Runtime.PYTHON_3_7,
+                                        # selecting runtime for lambda code
+                                        handler="handler.main",
+                                        # where to find lambda handler
+                                        code=aws_lambda.Code.asset("./lambda")
+                                        # application code tied to lambda
+                                        )
+
+        #express intent - grant permisions in construct library such as IAM Policy solves permissions problem
+        table.grant_read_write_data(function)
+
+        # adding env var -- only creating desired state representation -- token system in cdk for late binding @ provisioning-- forward preference
+        function.add_environment("TABLE_NAME", table.table_name)
+
+# cdk diff --profile workProfile -- to check difference between  local env and deployed
